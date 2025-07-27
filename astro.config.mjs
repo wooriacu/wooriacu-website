95ffaad59a635bba2946a2fbe724171635fd6e07
Michael Kang <michael3dk@gmail.com>
2025-07-27 11:20:31 -0700

    chore: tweak the information

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wooriacu.github.io',
-  base: '/wooriacu-website',
-  outDir: './dist'
+  site: "https://wooriacu.github.io",
+  base: "/",
+  outDir: "./dist",
 });
